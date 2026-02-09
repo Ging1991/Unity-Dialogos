af9005ab1668c4f88916944fdf9904e17cd8ccfb
Ging1991 <carloscaballeromorel@gmail.com>
2026-02-09 02:21:13 -0300

    Nueva estructura

---
+++ Runtime/IGetImagen.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace Ging1991.Dialogos {
+
+	public interface IGetImagen {
+
+		Sprite GetImagen(string nombre);
+
+	}
+
+}
---
+++ Runtime/Personaje.cs
@@ -0,0 +1,16 @@
+using Ging1991.Animaciones.Efectos;
+using UnityEngine;
+
+namespace Ging1991.Dialogos {
+
+	public class Personaje : MonoBehaviour {
+
+		public Trasparentar aparecer;
+		public Trasparentar desaparecer;
+		public Escalar acercar;
+		public Escalar alejar;
+		public Mover mover;
+
+	}
+
+}
